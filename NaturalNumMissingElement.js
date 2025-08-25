8b15511b0a0b27a290f29f32bd74aa0d7eb5bf44
braju007 <erbrj7@gmail.com>
2025-08-25 23:06:13 +0530

    Did code pretty with prettier

--- NaturalNumMissingElement.js
+++ NaturalNumMissingElement.js
@@ -1,8 +1,8 @@
 function findMissingArrayElement(arr) {
-    const n = arr.length + 1;
-    const sum = (n * (n + 1)) / 2;
-    const arrSum = arr.reduce((a, b) => a + b);
-    return sum - arrSum;
+  const n = arr.length + 1;
+  const sum = (n * (n + 1)) / 2;
+  const arrSum = arr.reduce((a, b) => a + b);
+  return sum - arrSum;
 }
 const arr = [1, 2, 3, 4, 5, 6, 8];
 console.log(findMissingArrayElement(arr));
--- TwoSum.js
+++ TwoSum.js
@@ -1,14 +1,14 @@
-function twoSum(arr, target){
-    let map = new Map();
-    for(let i=0; i<arr.length; i++){
-        let paired = target - arr[i];
-        if(map.has(paired)){
-            return [map.get(paired), i];
-        }
-        map.set(arr[i], i);
+function twoSum(arr, target) {
+  let map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    let paired = target - arr[i];
+    if (map.has(paired)) {
+      return [map.get(paired), i];
     }
+    map.set(arr[i], i);
+  }
 }
-const arr = [2,3,4,5,6,7];
+const arr = [2, 3, 4, 5, 6, 7];
 console.log(twoSum(arr, 9)); // [ 2, 3 ]
 console.log(twoSum(arr, 15)); // undefined
 console.log(twoSum(arr, 13)); // [ 4, 5 ]
