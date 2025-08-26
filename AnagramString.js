66b668b98367e422dced87f1467a9393549850f5
braju007 <erbrj7@gmail.com>
2025-08-26 23:33:43 +0530

    Anagram string or not

---
+++ AnagramString.js
@@ -0,0 +1,8 @@
+function areAnagrams(str1, str2){
+    const word1 = str1.split('').sort().join('');
+    const word2 = str2.split('').sort().join('');
+    return word1 === word2;
+  }
+  console.log(areAnagrams("silent", "listen"));
+  // true
+  
