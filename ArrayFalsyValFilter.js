7af50d4fcafb45b55e912532d07d62f162fd9959
braju007 <erbrj7@gmail.com>
2025-08-27 17:59:36 +0530

    Remove falsy values in array

---
+++ ArrayFalsyValFilter.js
@@ -0,0 +1,6 @@
+function removeFalsyValues(array){
+	return array.filter(Boolean);
+}
+
+console.log(removeFalsyValues([1,0,undefined,null,false,true,{},[],![]]));
+// Array [1, true, Object {  }, Array []]
