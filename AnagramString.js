c3aae426465260492f8867aacf4944d4d4fd8d25
braju007 <erbrj7@gmail.com>
2025-08-28 23:28:58 +0530

    Run prettier to pretty the code

--- AnagramString.js
+++ AnagramString.js
@@ -1,8 +1,7 @@
-function areAnagrams(str1, str2){
-    const word1 = str1.split('').sort().join('');
-    const word2 = str2.split('').sort().join('');
-    return word1 === word2;
-  }
-  console.log(areAnagrams("silent", "listen"));
-  // true
-  
+function areAnagrams(str1, str2) {
+  const word1 = str1.split("").sort().join("");
+  const word2 = str2.split("").sort().join("");
+  return word1 === word2;
+}
+console.log(areAnagrams("silent", "listen"));
+// true
--- LongestWordInSentence.js
+++ LongestWordInSentence.js
@@ -1,13 +1,12 @@
-function findLongestWord(sentence){
-    const words = sentence.split(' ');
-    let longestWord='';
-    for(let word of words){
-      if(word.length > longestWord.length){
-        longestWord = word;
-      }
+function findLongestWord(sentence) {
+  const words = sentence.split(" ");
+  let longestWord = "";
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
     }
-    return longestWord
   }
-  console.log(findLongestWord("I am Raju from Yaragatti village"));
-  // Yaragatti
-  
+  return longestWord;
+}
+console.log(findLongestWord("I am Raju from Yaragatti village"));
+// Yaragatti
