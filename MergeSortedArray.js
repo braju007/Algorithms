d7d9ef81f3932bb66632fa204ded9e361e7bf937
braju007 <erbrj7@gmail.com>
2025-09-02 22:54:54 +0530

    First non repeat character

---
+++ MergeSortedArray.js
@@ -0,0 +1,20 @@
+function mergeSortedArrays(arr1, arr2) {
+  const merged = [];
+  let i = 0,
+    j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+  return merged.concat(arr1.slice(i)).concat(arr2.slice(j));
+}
+
+const arr1 = [1, 3, 5];
+const arr2 = [2, 4, 6];
+console.log(mergeSortedArrays(arr1, arr2));
+// [1, 2, 3, 4, 5, 6]
