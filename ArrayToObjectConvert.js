e02ed65366529e9ddeb29b2fe7cc41af7e06902b
braju007 <erbrj7@gmail.com>
2025-09-06 23:36:16 +0530

    Convert array to object

---
+++ ArrayToObjectConvert.js
@@ -0,0 +1,10 @@
+function arrayToObj(arr){
+    return arr.reduce((obj, [key, value])=>{
+      obj[key] = value;
+      return obj;
+    },{})
+  }
+  
+  console.log(arrayToObj([["a", 1], ["b", 2]]));
+  // { a: 1, b: 2 }
+  
